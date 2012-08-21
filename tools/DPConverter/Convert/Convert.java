def4d86bef3bf50fba061ed5de52eab134c95fb6
Sharvil Nanavati <sharvil.nanavati@gmail.com>
2012-08-20 23:22:08 -0700

    Update to Derek's latest code.

--- tools/DPConverter/Convert/Convert.java
+++ tools/DPConverter/Convert/Convert.java
@@ -9,17 +9,19 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Convert {
     //Declare the variable settings as a HashMap to read in the settings
     static Map<String, String> settings = new LinkedHashMap<String, String>();
-    static Map<String, String> sections = new LinkedHashMap<String, String>();
+    static Map<String, Map<String, String>> sections = new LinkedHashMap<String, Map<String, String>>();
 
     /**
     * DotProduct Settings Converter
     * @author Derek Marr
+    * @email dezmond.marr@Gmail.com
     */
     public static void main(String[] args) {
         System.out.println("#### DotProduct Settings Converter");
@@ -45,26 +47,26 @@ public class Convert {
                 String e = "";
                 while ((e = input.readLine()) != null) {
                     if (e.startsWith("[")) {
-                        if(e.equals("[Warbird]") || e.equals("[Javelin]") || e.equals("[Spider]") || e.equals("[Leviathan]") || e.equals("[Terrier]") || e.equals("[Weasal]") || e.equals("[Lancaster]") || e.equals("[Shark]")){
-                        Pattern p = Pattern.compile("\\[(.*?)\\]");
+                        Pattern p = Pattern.compile("\\[(.*)\\]");
                         Matcher m = p.matcher(e);
 
-                        while(m.find()) {
+                        while (m.find()) {
                             String currentSection = m.group(1);
-                            sections.put(currentSection, settings.toString());
-                            for (Map.Entry<String, String> entry : sections.entrySet()) {
+                            settings = new LinkedHashMap<String, String>();
+                            sections.put(currentSection, settings);
+                            /*for (Entry<String, Map<String, String>> entry : sections.entrySet()) {
                                 String keys = entry.getKey();
-                                String values = entry.getValue();
+                                Map<String, String> values = entry.getValue();
                                 System.out.println(keys + values);
-                            }
+                            }*/
                         }
-                        }
+
                     } else {
                         int bvalue = e.indexOf("=");
                         String value = e.substring(bvalue + 1);
                         int bkey = e.lastIndexOf("=");
                         String key = e.substring(0, bkey + 1);
-                        settings.put(key.replace("=", ":"), value);
+                        settings.put(key.replace("=", ""), value);
                     }
                 }
 
@@ -90,8 +92,20 @@ public class Convert {
                 System.out.println("File Created: settings.json");
                 FileWriter fstream = new FileWriter("settings.json");
                 BufferedWriter out = new BufferedWriter(fstream);
-                String mapname = settings.get("MapName:");
-                String owner = settings.get("Maker:");
+
+                String mapname = sections.get("Notes").get("MapName");
+                String owner = sections.get("Maker").get("Maker");
+                String kp = sections.get("Kill").get("KillPointsPerFlag");
+                String spd = sections.get("Misc").get("SendPositionDelay");
+                String fspd = sections.get("Misc").get("ExtraPositionData");
+                String width = "1024";
+                String height = "1024";
+                String sradius = "500";
+                String pcount = sections.get("Prize").get("MultiPrizeCount");
+                String prize = "0";
+                String gupgrade = sections.get("PrizeWeight").get("Gun");
+                String fullcharge = sections.get("PrizeWeight").get("Recharge");
+                String bounceb = sections.get("PrizeWeight").get("BouncingBullets");
 
                 System.out.println("Attempting to write settings to the new file...");
 
@@ -100,13 +114,46 @@ public class Convert {
                 out.write("# Map Name: " + mapname);
                 out.newLine();
                 out.write("# Creator: " + owner);
+                out.newLine();
 
-                for (Map.Entry<String, String> entry : settings.entrySet()) {
+                out.write("{");
+                out.newLine();
+                out.write("\"game\":{");
+                out.newLine();
+                out.write("\"killPoints\": " + kp + "\n}");
+                out.newLine();
+                out.write("\"network\":{");
+                out.newLine();
+                out.write("\"sendPositionDelay\": " + spd + "\n");
+                out.write("\"fastSendPositionDelay\": " + fspd + "\n}");
+                out.newLine();
+                out.newLine();
+                out.write("\"map\":{");
+                out.newLine();
+                out.write("\"width\": " + width + "\n");
+                out.write("\"height\": " + height + "\n");
+                out.write("\"spawnRadius\": " + sradius + "\n}");
+                out.newLine();
+                out.newLine();
+                out.write("\"prize\":{");
+                out.newLine();
+                out.write("\"count\": " + pcount + "\n");
+                out.write("\"weights\": [" + prize + "," + gupgrade + "," + fullcharge + "," + bounceb + "]" + "\n}");
+                out.newLine();
+                out.newLine();
+
+                for (Entry<String, Map<String, String>> entry : sections.entrySet()) {
                     String keys = entry.getKey();
-                    String values = entry.getValue();
+                    Map<String, String> values = entry.getValue();
+                    String e = keys;
 
-                    out.write(keys + values);
-                    out.newLine();
+                    if (e.equals("Warbird") || e.equals("Javelin") || e.equals("Spider") || e.equals("Leviathan") || e.equals("Terrier")
+                            || e.equals("Weasal") || e.equals("Lancaster") || e.equals("Shark")) {
+                        String shipName = e;
+                        out.write("\"ships\":");
+                        getShipSettings(shipName, out);
+
+                    }
 
                 }
                 System.out.println("The file has been successfully created with the settings in it.");
@@ -121,4 +168,84 @@ public class Convert {
 
     }
 
+    private static void getShipSettings(String shipName, BufferedWriter out) {
+        String name = shipName;
+        String xrad = sections.get(name).get("Radius");
+        String yrad = sections.get(name).get("Radius");
+        String bouncef = sections.get(name).get("BounceFactor");
+        String rotrad = sections.get(name).get("InitialRotation");
+        String speedPix = sections.get(name).get("InitialSpeed");
+        String maxEn = sections.get(name).get("MaximumEnergy");
+        String accTick = sections.get(name).get("MaximumThrust");
+        String afterSpeed = sections.get(name).get("MaximumSpeed");
+        String afterAcc = sections.get(name).get("MaximumThrust");
+        String afterEn = sections.get(name).get("AfterburnerEnergy");
+        String rechargeRate = sections.get(name).get("InitialRecharge");
+        String respawnDelay = "500";
+        String fireEnergy = sections.get(name).get("BulletFireEnergy");
+        String fireEnergyUp = sections.get(name).get("BombFireUpgrade");
+        String speed = sections.get(name).get("BulletSpeed");
+        String fireDelay = sections.get(name).get("BulletFireDelay");
+        String lifetime = sections.get(name).get("BulletAliveTime");
+        String damage = sections.get("Bullet").get("ExactDamage");
+        String damageUpgrade = sections.get("Bullet").get("BulletDamageUpgrade");
+        String maxLevel = sections.get(name).get("BulletDamageLevel");
+        String bounce = "false";
+        String blastRadius = sections.get("Bomb").get("ProximityDistance");
+        String blastRadiusUp = sections.get(name).get("ProximityDistance");
+        String bounceCount = sections.get(name).get("BombBounceCount");
+        String recoilAcceleration = sections.get(name).get("Radius");
+
+        try {
+
+            out.newLine();
+            out.write("[");
+            out.newLine();
+            out.write("{");
+            out.newLine();
+            out.write("\"name\": " + name + "\n");
+            out.write("\"xRadius\": " + xrad + "\n");
+            out.write("\"yRadius\": " + yrad + "\n");
+            out.write("\"bounceFactor\": " + bouncef + "\n");
+            out.write("\"rotationRadiansPerTick\": " + rotrad + "\n");
+            out.write("\"speedPixelsPerTick\": " + speedPix + "\n");
+            out.write("\"maxEnergy\": " + maxEn + "\n");
+            out.write("\"accelerationPerTick\": " + accTick + "\n");
+            out.write("\"afterburnerMaxSpeed\": " + afterSpeed + "\n");
+            out.write("\"afterburnerAcceleration\": " + afterAcc + "\n");
+            out.write("\"afterburnerEnergy\": " + afterEn + "\n");
+            out.write("\"rechargeRate\": " + rechargeRate + "\n");
+            out.write("\"respawnDelay\": " + respawnDelay + "\n");
+            out.write("\"bullet\":{");
+            out.write("\"fireEnergy\": " + fireEnergy + "\n");
+            out.write("\"speed\": " + speed + "\n");
+            out.write("\"fireDelay\": " + fireDelay + "\n");
+            out.write("\"lifetime\": " + lifetime + "\n");
+            out.write("\"damage\": " + damage + "\n");
+            out.write("\"damageUpgrade\": " + damageUpgrade + "\n");
+            out.write("\"maxlLevel\": " + maxLevel + "\n");
+            out.write("\"bounces\": " + bounce + "\n}");
+            out.newLine();
+            out.write("\"bomb\":{");
+            out.newLine();
+            out.write("\"fireEnergy\": " + fireEnergy + "\n");
+            out.write("\"fireEnergyUpgrade\": " + fireEnergyUp + "\n");
+            out.write("\"speed\": " + speed + "\n");
+            out.write("\"fireDelay\": " + fireDelay + "\n");
+            out.write("\"lifetime\": " + lifetime + "\n");
+            out.write("\"damage\": " + damage + "\n");
+            out.write("\"damageUpgrade\": " + damageUpgrade + "\n");
+            out.write("\"blastRadius\": " + blastRadius + "\n");
+            out.write("\"blastRadiusUpgrade\": " + blastRadiusUp + "\n");
+            out.write("\"bounceCount\": " + bounceCount + "\n");
+            out.write("\"recoilAcceleraion\": " + name + "\n");
+            out.write("}");
+            out.newLine();
+            out.write("},");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
