698d438150ce39ff035caa59ab6aaacd6e45126f
Sharvil Nanavati <sharvil.nanavati@gmail.com>
2015-04-06 00:29:10 -0700

    Add externs file to reduce compiler warnings about undefined symbols.

---
+++ js/externs.js
@@ -0,0 +1,19 @@
+/**
+ * @constructor
+ */
+var AudioContext = function() {};
+AudioContext.prototype.destination;
+AudioContext.prototype.createBufferSource = function() {};
+AudioContext.prototype.decodeAudioData = function(audioData, successCallback, errorCallback) {};
+
+/**
+ * @param {string} queryString
+ * @return {!Object}
+ */
+window.toObject = function(queryString) {};
+
+/**
+ * @param {string} str
+ * @return {string}
+ */
+window.linkify = function(str) {};
