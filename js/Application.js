f144e549b32c070b28856dc71fc23169a2b356ce
Sharvil Nanavati <sharvil.nanavati@gmail.com>
2017-01-07 11:41:28 -0800

    Use secure websockets if application is delivered over SSL.

--- js/Application.js
+++ js/Application.js
@@ -91,6 +91,10 @@ Application.prototype.onLoadComplete_ = function() {
 
 var _main = function() {
   var settings = window.toObject(window.location.hash.substr(1));
-  new Application(settings, 'ws://' + window.location.host + '/dotproduct/v1/' + settings['arena']);
+  var socketUri = 'ws://' + window.location.host + '/dotproduct/v1/' + settings['arena'];
+  if (window.location.protocol == 'https:') {
+    socketUri = socketUri.replace('ws://', 'wss://');
+  }
+  new Application(settings, socketUri);
   window.location.hash = '';
 };
